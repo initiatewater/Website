fa0be7d18cbf49236e9c5595c9a4995fa0fbc1f7
Initiatewater <initiate.water@gmail.com>
2026-07-26 13:34:22 +0200

    changed sanity integration

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,6 +1,31 @@
-import doctorant from './doctorant'
-import universite from './universite'
-import publication from './publication'
-import faq from './faq'
+import doctorant       from './doctorant'
+import universite      from './universite'
+import publication     from './publication'
+import faq             from './faq'
+import siteSettings    from './siteSettings'
+import pageHome        from './pageHome'
+import pagePodcasts    from './pagePodcasts'
+import pagePublications from './pagePublications'
+import pageContact     from './pageContact'
+import pageApply       from './pageApply'
 
-export const schemaTypes = [doctorant, universite, publication, faq]
+/*
+  Ordre d'affichage dans Sanity Studio.
+  Les documents "Page*" et "Paramètres" sont en haut pour
+  que l'admin les trouve facilement.
+*/
+export const schemaTypes = [
+  // Paramètres globaux
+  siteSettings,
+  // Contenus de pages
+  pageHome,
+  pagePodcasts,
+  pagePublications,
+  pageContact,
+  pageApply,
+  // Données
+  doctorant,
+  universite,
+  publication,
+  faq,
+]
